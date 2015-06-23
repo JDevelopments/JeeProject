dd86d070e015a88183bd060191008a64e3cf46b4
ziper <ziper@comp>
2015-06-24 01:26:42 +0300

    Hibernate Vork!!!

--- Timex/src/main/java/ua/ziper/timex/test/HibernateTest.java
+++ Timex/src/main/java/ua/ziper/timex/test/HibernateTest.java
@@ -2,12 +2,7 @@ package ua.ziper.timex.test;
 
 
 import java.util.List;
-
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
-
 import ua.ziper.timex.model.Department;
 import ua.ziper.timex.persistent.*;
 
@@ -16,10 +11,10 @@ public class HibernateTest {
 	
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		session.beginTransaction();
-		
 		Department department = new Department();
 	
-		List list = session.createQuery("from Department").list();
+		List<Department> list = session.createQuery("from Department").list();
+	    
 		for (int i = 0; i < list.size(); i++) {
 			department = (Department) list.get(i);
 			System.out.println(i+") " + department.getName()+" "+department.getDepartmentId());
